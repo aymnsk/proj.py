88f9df7b0720ac940d016d26bcea7d1a2a1a310f
Ayman <168225570+aymnsk@users.noreply.github.com>
2025-10-01 18:02:24 +0530

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,23 @@
+import random
+
+flashcards = {
+    "Capital of India?": "Delhi",
+    "2+2?": "4",
+    "Python keyword for function?": "def"
+}
+
+def quiz():
+    score = 0
+    questions = list(flashcards.keys())
+    random.shuffle(questions)
+
+    for q in questions:
+        ans = input(q + " ")
+        if ans.lower() == flashcards[q].lower():
+            print("✅ Correct")
+            score += 1
+        else:
+            print("❌ Wrong | Answer:", flashcards[q])
+    print("\nFinal Score:", score, "/", len(questions))
+
+quiz()
